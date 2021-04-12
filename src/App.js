42319a9338903dde1b19f74119c34a4e93faabdc
Debora Durek <debora.durek@gmail.com>
2021-04-11 22:41:08 -0500

    feat: Add ChatWindow element to view

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ChatWindow from './ChatWindow';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">React Nanodegree - Coding Practice by Udacity</h1>
+    </header>
+
+    <ChatWindow />
+  </div>
+);
 
 export default App;
