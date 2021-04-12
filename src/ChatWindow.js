8bd6f4fe9b439941b0a463c1a0eab47bdda806d2
Debora Durek <debora.durek@gmail.com>
2021-04-11 22:51:27 -0500

    feat: Add map over users to create chat windows

--- src/ChatWindow.js
+++ src/ChatWindow.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import MessageList from './MessageList';
+import SendMessage from './SendMessage';
 
 class ChatWindow extends Component {
   state = {
@@ -6,11 +8,21 @@ class ChatWindow extends Component {
   };
 
   render() {
+    const users = [{ username: 'Amy' }, { username: 'John' }];
     const { messages } = this.state;
 
     return (
       <div className="container">
-        <h2>Super Awesome Chat</h2>
+        {users.map((user) => (
+          <div key={`chat-window-${user.username}`} className="chat-window">
+            <h2>Super Awesome Chat</h2>
+            <div className="name sender">{user.username}</div>
+
+            <MessageList messages={messages} user={user} />
+
+            <SendMessage />
+          </div>
+        ))}
       </div>
     );
   }
