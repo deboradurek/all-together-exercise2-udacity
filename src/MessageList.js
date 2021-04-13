1a9d2cb3e9798aee01daf95ea3a5fa704a1c6a36
Debora Durek <debora.durek@gmail.com>
2021-04-12 19:35:21 -0500

    feat: Create list of messages to view

--- src/MessageList.js
+++ src/MessageList.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const MessageList = () => {
-  return <ul className="message-list"></ul>;
+const MessageList = ({ messages, user }) => {
+  return (
+    <ul className="message-list">
+      {messages.map((message, index) => (
+        <li
+          key={`${message.username}-${index}`}
+          className={message.username === user.username ? 'message sender' : 'message recipient'}
+        >
+          <p>{`${message.username}: ${message.text}`}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+MessageList.propTypes = {
+  messages: PropTypes.array.isRequired,
+  user: PropTypes.object.isRequired,
 };
 
 export default MessageList;
